491a152bfa0ab7d4c33c223a2f0fbb21894fac99
Yudi Candra <yudi.c@mhs.unsyiah.ac.id>
2022-04-21 15:58:43 +0700

    Fix Error

--- redoc/lib/pilihdokter.dart
+++ redoc/lib/pilihdokter.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:redoc/anak.dart';
 import 'package:redoc/beranda.dart';
+import 'package:redoc/jantung.dart';
 import 'package:redoc/kandungan.dart';
 import 'package:redoc/paru.dart';
-<<<<<<< HEAD
+
 import 'package:redoc/penyakitdalam.dart';
 import 'package:redoc/penyakitjiwa.dart';
-=======
+
 import 'package:redoc/penyakitsaraf.dart';
 import 'package:redoc/penyakittht.dart';
->>>>>>> 1904111010030
+
 import 'package:redoc/utama.dart';
 
 class PilihDokter extends StatefulWidget {
@@ -212,7 +214,7 @@ class _PilihDokterState extends State<PilihDokter> {
                             Navigator.push(
                               context,
                               new MaterialPageRoute(
-                                  builder: (context) => new Home()),
+                                  builder: (context) => new PenyakitJantung()),
                             );
                           },
                           icon: Image(image: AssetImage('assets/jantung.png')),
