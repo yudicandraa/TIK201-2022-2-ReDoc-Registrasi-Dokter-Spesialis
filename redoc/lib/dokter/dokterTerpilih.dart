3522cfbc18e30673fd9dd050604c38783dda04e7
win putra sandi agung <win_psa20@mhs.unsyiah.ac.id>
2022-05-08 15:17:25 +0700

    buat database antrian

---
+++ redoc/lib/dokter/dokterTerpilih.dart
@@ -0,0 +1,20 @@
+class DokterTerpilih {
+  int? antrian;
+  Map? dokter;
+  Map? pasien;
+
+  DokterTerpilih({this.antrian, this.dokter, this.pasien});
+
+  factory DokterTerpilih.fromMap(map) {
+    return DokterTerpilih(
+        antrian: map['antrian'], dokter: map['dokter'], pasien: map['pasien']);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'antrian': antrian,
+      'dokter': dokter,
+      'pasien': pasien,
+    };
+  }
+}
