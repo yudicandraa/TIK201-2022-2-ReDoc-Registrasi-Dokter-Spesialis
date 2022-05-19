b0b16bb4f5706340066b2d03ceb06028a1989fd4
Khairuzzikri <khairuzz@mhs.unsyiah.ac.id>
2022-05-19 23:50:41 +0700

    Fix

--- redoc/ios/Flutter/flutter_export_environment.sh
+++ redoc/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Project\ReDoc\redoc"
+export "FLUTTER_ROOT=D:\aa\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\MATA KULIAH SEMESTER 6\REKAYASA PERANGKAT LUNAK (12) Maya Fitria, S.Kom., M.Sc\GIT\TIK201-2022-2-ReDoc-Registrasi-Dokter-Spesialis\redoc"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
